52b59722269d4dd2b47ecd265d808dd5b63010a7
nastasia <IU3Labs@ya.ru>
2026-09-25 21:46:33 +0300

    Add docs and example

---
+++ 00-example-module/src/test/java/ru/bmstu/MainTest.java
@@ -0,0 +1,14 @@
+package ru.bmstu;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        Assertions.assertTrue(5 < 10);
+    }
+}
